a1795666df043374378fc221cfd2e9f837b98f08
Alex Du Bois <alex.dubois@mac.com>
2021-08-25 11:18:30 -0500

    Add ReactiveSwiftCombine
    Signed-off-by: Alex Du Bois <alex.dubois@mac.com>

---
+++ Sources/ReactiveSwiftCombine/ReactiveSwiftPublisher.swift
@@ -0,0 +1,44 @@
+import Combine
+import ReactiveSwift
+
+public struct ReactiveSwiftPublisher<Output, Failure: Error>: Publisher {
+
+    private let signalProducer: SignalProducer<Output, Failure>
+
+    init(signalProducer: SignalProducer<Output, Failure>) {
+        self.signalProducer = signalProducer
+    }
+
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        let subscription = ReactiveSwiftSubscription(signalProducer: signalProducer, subscriber: subscriber)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+class ReactiveSwiftSubscription<S, Output, Failure>: Subscription where S: Subscriber, S.Input == Output, S.Failure == Failure {
+
+    private let signalProducer: SignalProducer<Output, Failure>
+
+    private let subscriber: S
+    private let disposable: Disposable
+
+    init(signalProducer: SignalProducer<Output, Failure>, subscriber: S) {
+        self.signalProducer = signalProducer
+        self.subscriber = subscriber
+
+        disposable = signalProducer.start { [subscriber] event in
+            switch event {
+            case .interrupted: subscriber.receive(completion: .finished)
+            case .completed: subscriber.receive(completion: .finished)
+            case .failed(let error): subscriber.receive(completion: .failure(error))
+            case .value(let value): _ = subscriber.receive(value)
+            }
+        }
+    }
+
+    func request(_ demand: Subscribers.Demand) { }
+
+    func cancel() {
+        disposable.dispose()
+    }
+}
